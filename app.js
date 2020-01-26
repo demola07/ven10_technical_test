55c7eb858048cca4b516e038f407f0e9a3595c49
demola07 <45898468+demola07@users.noreply.github.com>
2020-01-26 20:54:30 +0100

    handle cors, routes, middleware, models and controllers setup

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const app = require('express')();
 const connectDB = require('./config/db');
+const productRouter = require('./routes/product');
 
 // Database Connection
 connectDB();
+
+//CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
 
 app.use((req, res) => {
   res.json({
     message: 'Everything is working well'
   });
 });
+
+app.use('api/product', productRouter);
 
 module.exports = app;
---
+++ models/product.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  id: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  color: {
+    type: String,
+    required: true
+  },
+  productImage: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Product', productSchema);

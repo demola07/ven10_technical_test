4a2039c4310ffc5ae4390f447c8b3fd1829616a5
demola07 <45898468+demola07@users.noreply.github.com>
2020-01-27 20:52:19 +0100

    created product form template

--- client/src/components/productForm/productForm.js
+++ client/src/components/productForm/productForm.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export class productForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
     return (
-      <div>
-        <h2>Product Form</h2>
+      <div className='overlay'>
+        <div className='head'>
+          <h1>Add a Product </h1>
+          <small>*All fields are compulsory</small>
+        </div>
+        <form>
+          <input type='text' name='id' placeholder='Enter id' /> <br />
+          <input type='text' name='name' placeholder='Name of Product' />
+          <br />
+          <input
+            type='text'
+            name='description'
+            placeholder='Enter Description of Product'
+          />
+          <br />
+          <input
+            type='number'
+            name='price'
+            placeholder='Enter Price of Product ($)'
+          />
+          <br />
+          <input
+            type='text'
+            name='category'
+            placeholder='Enter Category Product Belongs to'
+          />
+          <br />
+          <input type='text' name='color' placeholder='Enter Product color' />
+          <br />
+          <div className='file'>
+            <input type='file' id='cname' name='cardname' />
+          </div>
+          <input type='submit' value='Post' />
+        </form>
       </div>
     );
   }
